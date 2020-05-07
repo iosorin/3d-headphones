9353366fe96b0071d8c77c6f8709e0632b9a417f
iosorin <miroslav.osorin@gmail.com>
2020-05-07 21:57:15 +0200

    :sparkles: medium tutorial branch

---
+++ README.md
@@ -0,0 +1,9 @@
+# 3D Headphones Configurator
+
+3D Headphones Configurator with THREE.js.
+
+Contains customizable colors for different areas of the model.
+
+Just upload your model to the static/model folder and enjoy.
+
+![](preview/0.gif)
---
+++ src/components/Scene/Scene.options.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="options">
+        <v-tooltip top color="black">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    x-small
+                    fab
+                    v-on="on"
+                    class="ml-2"
+                    @click="$emit('setActiveMesh', { forward: false })"
+                >
+                    <v-icon>mdi-ray-end-arrow</v-icon>
+                </v-btn>
+            </template>
+            <span>Previous headphone area</span>
+        </v-tooltip>
+
+        <v-tooltip top color="black">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    x-small
+                    fab
+                    v-on="on"
+                    class="ml-1 mr-2"
+                    @click="$emit('setActiveMesh')"
+                >
+                    <v-icon>mdi-ray-start-arrow</v-icon>
+                </v-btn>
+            </template>
+            <span>Next headphone area</span>
+        </v-tooltip>
+
+        <v-tooltip top color="black">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    x-small
+                    fab
+                    v-on="on"
+                    @click="$emit('toggleWireframes')"
+                >
+                    <v-icon>{{ showWireframes ? 'mdi-eye-off' : 'mdi-eye' }}</v-icon>
+                </v-btn>
+            </template>
+            <span>{{ showWireframes ? ' Hide' : ' Show' }} wireframes</span>
+        </v-tooltip>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['showWireframes']
+};
+</script>
+
+<style lang="scss">
+.options {
+    display: flex;
+    position: absolute;
+    bottom: 12px;
+    left: 14px;
+    right: 14px;
+}
+</style>
