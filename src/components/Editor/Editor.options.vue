c8c6aa5a51fa00ac50bfb2246de3604565c270e9
iosorin <miroslav.osorin@gmail.com>
2020-05-07 21:47:16 +0200

    3d-product configurator

---
+++ src/components/Editor/Editor.options.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="options">
+        <v-tooltip top color="black">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    x-small
+                    fab
+                    class="ml-auto"
+                    v-on="on"
+                    @click="$emit('applyColor')"
+                >
+                    <v-icon>mdi-rotate-right</v-icon>
+                </v-btn>
+            </template>
+            <span>Change color</span>
+        </v-tooltip>
+    </div>
+</template>
